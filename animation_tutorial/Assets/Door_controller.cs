910e28c095aee6e2995747080029404b41e104a8
Nick Beard <nb234515@ohio.edu>
2017-02-28 13:23:09 -0500

    Door Animation

---
+++ animation_tutorial/Assets/Door_controller.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door_controller : MonoBehaviour {
+
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void OnGUI () {
+
+		if (GUILayout.Button ("Open Door")) 
+		{
+			animator.SetInteger ("Door Close", 0);
+			animator.SetInteger ("Door Open", 1);
+
+		}
+		if (GUILayout.Button ("Close Door")) 
+		{
+			animator.SetInteger ("Door Close", 1);
+			animator.SetInteger ("Door Open", 0);
+		}
+
+
+	}
+}
